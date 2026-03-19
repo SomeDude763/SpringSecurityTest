a7c2957b0bfc51225a9a64fc18b1142bd9955f8c
SomeDude763 <misterklokinski@gmail.com>
2026-03-19 23:02:48 +0400

    работа ведется

--- src/main/java/org/example/springsecuritytest/config/SecurityConfig.java
+++ src/main/java/org/example/springsecuritytest/config/SecurityConfig.java
@@ -1,37 +1,45 @@
 package org.example.springsecuritytest.config;
 
-import org.example.springsecuritytest.security.AuthProviderImpl;
+import org.example.springsecuritytest.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.naming.AuthenticationException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
+    //настраиваем аутентификацию
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
-                .authenticationProvider(authProvider)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
 
--- src/main/java/org/example/springsecuritytest/controllers/HelloController.java
+++ src/main/java/org/example/springsecuritytest/controllers/HelloController.java
@@ -7,17 +7,19 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-return "Hello";
+        return "Hello";
     }
+
     @GetMapping("/showUserInfo")
     public String showUserInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-     PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         System.out.println(personDetails.getPerson());
 
         return "Hello";
